52dd730d4e3767224ba6e6ead8d17ca83a49a144
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-08-05 19:49:43 +0530

    added responsive code

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const form = document.forms['google-sheet'];
 
 const category_dropdown = document.getElementById('category-dropdown');
 
+// const dropbtn = document.getElementById('dropbtn');
+
 const category_toggle = document.getElementById('category-toggle');
 
 const category_clear= document.querySelectorAll('.category-clear');
@@ -14,8 +16,15 @@ const update_btn = document.getElementById('update-btn');
 
 const message = document.getElementById('message');
 
+const submit_btn = document.getElementById('submit-btn');
+
 
 category_dropdown.addEventListener('click', () => {
+
+    // dropbtn.addEventListener('click', () => {
+    //     category_toggle.style.display = 'none';
+    // });
+
     if(category_toggle.style.display === 'none') {
         
         category_toggle.style.display = 'block';
@@ -26,25 +35,35 @@ category_dropdown.addEventListener('click', () => {
     }
 });
 
+
+
 $('#category-dropdown').change(() =>{
     $('.category-clear').val('');
     $(".category-checkbox").prop("checked", false);
 }); 
 
 
-// Sending data to Google Sheet
+// Sending data to etails
 form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
     fetch(scriptUrl, {method: 'POST' , body: new FormData(form)})
-        .then(response => $('#form_alert').html("<div class='alert rounded text-center w-75 alert-success'>Google Sheet updated successfully!</div>"))
+        .then(response => $('#form_alert').html("<div class='alert rounded text-center w-75 alert-success'>Details updated successfully!</div>"))
         .catch(error => $('#message-container').html("<div class='alert alert-danger'>Update failed!</div>"))
         
     setTimeout(() => {
         $('#form_alert').html("");
     },5000)
+
+    submit_btn.style.display = 'block';
 });
+
+//Submit-btn 
+
+let reload = () => {
+    window.location.reload();
+};
 
 // checkBox
 let last;
